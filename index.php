bafbe5d24664253db62ab8a5516b512fc0a45d18
Daniel <danielpellegrini87@gmail.com>
2021-03-15 15:58:52 +0100

    Main exercise completed

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ class Animal
 
 
 
-$cat = new Animal('Eukarya', 'Animalia', 'Chordata', 'Mammalia', 'Carnivora', 'Felidae', 'Felis', 'F. catus');
+$cat = new Animal('Eukarya', 'Animalia', 'Chordata', 'Mammalia', 'Carnivora', 'Felidae', 'Felis', 'Felis catus');
 
 echo 'CAT <br> Scientific classification:<br><br>';
 echo 'Domain: ' . $cat->domain . '<br>';
@@ -45,3 +45,27 @@ echo 'Order: ' . $cat->order . '<br>';
 echo 'Family: ' . $cat->family . '<br>';
 echo 'Genus: ' . $cat->genus . '<br>';
 echo 'Species: ' . $cat->species . '<br>';
+
+$dog = new Animal('Eukarya', 'Animalia', 'Chordata', 'Mammalia', 'Carnivora', 'Canidae', 'Canis', 'Canis lupus familiaris');
+
+echo '<br>DOG <br> Scientific classification:<br><br>';
+echo 'Domain: ' . $dog->domain . '<br>';
+echo 'Kingdom: ' . $dog->kingdom . '<br>';
+echo 'Phylum: ' . $dog->phylum . '<br>';
+echo 'Class: ' . $dog->class . '<br>';
+echo 'Order: ' . $dog->order . '<br>';
+echo 'Family: ' . $dog->family . '<br>';
+echo 'Genus: ' . $dog->genus . '<br>';
+echo 'Species: ' . $dog->species . '<br>';
+
+$horse = new Animal('Eukarya', 'Animalia', 'Chordata', 'Mammalia', 'Perissodactyla', 'Equidae', 'Equus', 'Equus ferus caballus');
+
+echo '<br>HORSE <br> Scientific classification:<br><br>';
+echo 'Domain: ' . $horse->domain . '<br>';
+echo 'Kingdom: ' . $horse->kingdom . '<br>';
+echo 'Phylum: ' . $horse->phylum . '<br>';
+echo 'Class: ' . $horse->class . '<br>';
+echo 'Order: ' . $horse->order . '<br>';
+echo 'Family: ' . $horse->family . '<br>';
+echo 'Genus: ' . $horse->genus . '<br>';
+echo 'Species: ' . $horse->species . '<br>';
